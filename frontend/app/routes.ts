1670952afb8b7fec671bacd36348057f1ef57adb
ryunosukesaito-0523 <ryunosuke.saito@g.dreamcareer.co.jp>
2026-05-14 14:25:15 +0900

    feat: ログイン機能と各種画面のファイルを追加

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,5 +1,12 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/login.tsx"),
+
+  layout("layouts/auth-guard.tsx", [
+    route("top", "routes/top.tsx"),
+    route("create-question", "routes/createQuestion.tsx"),
+    route("question/:id", "routes/question.tsx"),
+    route("profile", "routes/profile.tsx"),
+  ]),
 ] satisfies RouteConfig;
---
+++ frontend/app/routes/top.tsx
@@ -0,0 +1,6 @@
+
+export default function Top() {
+    return (
+        <div >TOP</div>
+    );
+}
