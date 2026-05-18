963dfb0eb81f0cccbfe9d46c5faa46cf1db8d052
ryunosukesaito-0523 <ryunosuke.saito@g.dreamcareer.co.jp>
2026-05-18 15:40:39 +0900

    feat: tagChipコンポーネント実装

--- frontend/app/components/common/TagChip.tsx
+++ frontend/app/components/common/TagChip.tsx
@@ -2,12 +2,12 @@
 
 
 type TagChipProps = {
-    name: string;
+    text: string;
 };
-export default function TagChip({ name }: TagChipProps) {
-
+export default function TagChip({ text }: TagChipProps) {
     return (
-        <div >
+        <div className="rounded-[var(--radius-big)] px-[var(--spacing-16)] py-[var(--spacing-8)] bg-white inline-flex border border-[var(--accent-color)]" >
+            <p className="flex items-center font-['Lora'] text-[var(--accent-color)] h-[var(--spacing-12)]">{text}</p>
         </div>
     );
 }
