14354c14ae3fdeacb6f66fbb5a680276d0dc4f82
ryunosukesaito-0523 <ryunosuke.saito@g.dreamcareer.co.jp>
2026-05-18 14:51:02 +0900

    feat: カードにclassnameを指定できるように追加

--- frontend/app/components/common/Card.tsx
+++ frontend/app/components/common/Card.tsx
@@ -2,11 +2,12 @@ import type { ReactNode } from "react";
 
 type CardProps = {
     children: ReactNode;
+    className?: string;
 };
 
-export default function Card({ children }: CardProps) {
+export default function Card({ children, className }: CardProps) {
     return (
-        <div className="bg-white shadow-[var(--box-shadow)] rounded-[var(--radius-big)] p-4">
+        <div className={`bg-white shadow-[var(--box-shadow)] rounded-[var(--radius-big)] p-4 ${className || ""}`}>
             {children}
         </div>
     );
