8c6dee4166da6343c513755b37ce5fc4fef143c9
ryunosukesaito-0523 <ryunosuke.saito@g.dreamcareer.co.jp>
2026-05-18 14:42:36 +0900

    feat: buttonコンポーネント実装

---
+++ frontend/app/components/common/Button.tsx
@@ -0,0 +1,14 @@
+
+type ButtonProps = {
+    text: string;
+    onClick: () => void;
+};
+
+export default function Button({ text, onClick }: ButtonProps) {
+
+    return (
+        <button onClick={onClick} className="shadow-[var(--box-shadow)] inline-block px-[var(--spacing-128)] py-[var(--spacing-16)] bg-[image:var(--gradation-green)] text-white rounded-[var(--radius-small)] cursor-pointer">
+            {text}
+        </button>
+    );
+}
