a1ea3aea549a90d53bb3a51a61c232ccb4364313
ryunosukesaito-0523 <ryunosuke.saito@g.dreamcareer.co.jp>
2026-05-18 18:03:59 +0900

    feat: SectionTitleコンポーネント実装

--- frontend/app/components/common/SectionTitle.tsx
+++ frontend/app/components/common/SectionTitle.tsx
@@ -3,11 +3,16 @@
 
 type SectionTitleProps = {
     title: string;
+    children?: React.ReactNode;
 };
-export default function SectionTitle({ title }: SectionTitleProps) {
+export default function SectionTitle({ title, children }: SectionTitleProps) {
 
     return (
-        <div >
+        <div className="py-[var(--spacing-16)]">
+            <div className="flex py-[var(--spacing-16)] justify-between items-center border-b border-[var(--main-color)]">
+                <p className="font-[var(--font-size-big)]">{title}</p>
+                {children}
+            </div>
         </div>
     );
 }
