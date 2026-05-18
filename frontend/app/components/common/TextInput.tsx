d9b7389e276f800ca972c28ac44e58e5f6bb8801
ryunosukesaito-0523 <ryunosuke.saito@g.dreamcareer.co.jp>
2026-05-18 18:50:04 +0900

    feat: textInputで入力した値が取得できる機能を追加

--- frontend/app/components/common/TextInput.tsx
+++ frontend/app/components/common/TextInput.tsx
@@ -2,33 +2,40 @@ import SearchIcon from '@mui/icons-material/Search';
 
 type TextInputProps = {
     placeholder: string;
-    isSearch?: boolean;    // 検索アイコンを表示するかどうかの判定
-    onSearch?: () => void; // 検索ボタンが押された時の処理
+    isSearch?: boolean;
+    onSearch?: () => void;
+    value: string;
+    onChange: (text: string) => void;
 };
 
-export default function TextInput({ placeholder, isSearch, onSearch }: TextInputProps) {
+export default function TextInput({ placeholder, isSearch, onSearch, value, onChange }: TextInputProps) {
     return (
         <div className="relative w-full">
             <input
                 type="text"
                 placeholder={placeholder}
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
                 className={`
                     w-full bg-white 
                     border border-[var(--light-gray)] rounded-[var(--radius-small)] 
-                    px-[var(--spacing-16)] py-[var(--spacing-8)]  
-                    
+                    px-[var(--spacing-16)] py-[var(--spacing-8)] 
                     text-[var(--text-color-black)] 
                     placeholder:text-[var(--dark-gray)] 
                     focus:outline-none focus:border-[var(--main-color)] focus:ring-1 focus:ring-[var(--main-color)] 
                     transition-all duration-200
                 `}
             />
+
             {isSearch && (
                 <button
                     type="button"
-                    onClick={onSearch}
+                    onClick={() => {
+                        if (value !== "") {
+                            onSearch?.();
+                        }
+                    }}
                     className={`
-                        cursor-pointer
                         absolute right-[var(--spacing-16)] top-1/2 -translate-y-1/2 
                         w-[24px] h-[24px] bg-[var(--main-color)] rounded-full 
                         flex items-center justify-center text-white 
