dac8ce1b3c0db27f6ffafa299f9c1b1de522ce64
ryunosukesaito-0523 <ryunosuke.saito@g.dreamcareer.co.jp>
2026-05-19 10:40:01 +0900

    fix: tagChipの高さが不安定なバグを修正

--- frontend/app/components/common/TagChip.tsx
+++ frontend/app/components/common/TagChip.tsx
@@ -8,7 +8,7 @@ type TagChipProps = {
 };
 export default function TagChip({ text, isButton, onClick }: TagChipProps) {
     return (
-        <div className={`rounded-[var(--radius-big)] px-[var(--spacing-16)] py-[var(--spacing-8)] bg-white inline-flex items-center border border-[var(--accent-color)] gap-[var(--spacing-4)] ${isButton ? "h-[32px]" : ""} `} >
+        <div className={`rounded-[var(--radius-big)] px-[var(--spacing-16)] py-[var(--spacing-8)] bg-white inline-flex items-center border border-[var(--accent-color)] gap-[var(--spacing-4)] h-[27px]`} >
             <p className="flex items-center font-['Lora'] text-[var(--accent-color)] h-[var(--spacing-12)]">{text}</p>
             {isButton && (
                 <CloseIcon className='flex items-center cursor-pointer !text-[16px] text-[var(--light-gray)] ' onClick={onClick} />
