869b325d152cc4c47ada69bde121185461fb98a9
ryunosukesaito-0523 <ryunosuke.saito@g.dreamcareer.co.jp>
2026-05-20 15:58:49 +0900

    fix: buttonコンポーネントに渡した関数が描画時に即実行されてしまう不具合を修正

--- frontend/app/components/common/Button.tsx
+++ frontend/app/components/common/Button.tsx
@@ -7,7 +7,7 @@ type ButtonProps = {
 export default function Button({ text, onClick }: ButtonProps) {
 
     return (
-        <button onClick={onClick} className="shadow-[var(--box-shadow)] inline-block px-[var(--spacing-128)] py-[var(--spacing-16)] bg-[image:var(--gradation-green)] text-white rounded-[var(--radius-small)] cursor-pointer">
+        <button onClick={()=>onClick} className="shadow-[var(--box-shadow)] inline-block px-[var(--spacing-128)] py-[var(--spacing-16)] bg-[image:var(--gradation-green)] text-white rounded-[var(--radius-small)] cursor-pointer">
             {text}
         </button>
     );
