a53e650ca98b558c202b99cbb41145b4f519a46e
ryunosukesaito-0523 <ryunosuke.saito@g.dreamcareer.co.jp>
2026-05-20 11:40:56 +0900

    chore:avatarコンポーネントの仕様変更

--- frontend/app/components/common/Avatar.tsx
+++ frontend/app/components/common/Avatar.tsx
@@ -1,11 +1,11 @@
 type AvatarProps = {
-    src: string;
-    alt?: string;
     //サイズは親要素で制御する
     className?: string;
 }
 
-export default function Avatar({ src, alt = "Avatar", className = "" }: AvatarProps) {
+export default function Avatar({ className = "" }: AvatarProps) {
+    const src = "https://t.pimg.jp/032/347/737/1/32347737.jpg";
+    const alt = "ユーザーのアバター";
     return (
         <div className={`rounded-full overflow-hidden flex-shrink-0 border-[1.5px] border-[var(--main-color)] ${className}`}>
             <img src={src} alt={alt} className="w-full h-full object-cover" />
