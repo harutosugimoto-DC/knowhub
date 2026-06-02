0c7a1dd727c8143a500802e9ea2603eb1135efd9
ryunosukesaito-0523 <ryunosuke.saito@g.dreamcareer.co.jp>
2026-06-02 10:29:12 +0900

    feat: Loadingコンポーネントにコンテキストからのデータで判断するように変更

--- frontend/app/components/common/Loading.tsx
+++ frontend/app/components/common/Loading.tsx
@@ -1,40 +1,31 @@
-type LoadingProps = {
-    isLoading: boolean;
-    message?: string; 
-};
+import { useLoading } from "@/contexts/LoadingContext";
 
-const Loading = ({ isLoading, message }: LoadingProps) => {
-    if (!isLoading) return
-
+const Loading = () => {
+    const { isLoading } = useLoading()
+    if (!isLoading) return null
     return (
         <div className="w-full h-full flex flex-col items-center justify-center gap-4 bg-[var(--base-color)] bg-opacity-70 backdrop-blur-sm animate-in fade-in duration-300">
-            
-            <svg 
-                className="animate-spin h-10 w-10 text-[var(--main-color)]" 
-                xmlns="http://www.w3.org/2000/svg" 
-                fill="none" 
+
+            <svg
+                className="animate-spin h-10 w-10 text-[var(--main-color)]"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
                 viewBox="0 0 24 24"
             >
-                <circle 
-                    className="opacity-25" 
-                    cx="12" 
-                    cy="12" 
-                    r="10" 
-                    stroke="currentColor" 
+                <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
                     strokeWidth="4"
                 ></circle>
-                <path 
-                    className="opacity-75" 
-                    fill="currentColor" 
+                <path
+                    className="opacity-75"
+                    fill="currentColor"
                     d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                 ></path>
             </svg>
-
-            {message && (
-                <p className="text-[length:var(--font-size-medium)] text-[var(--main-color)] font-medium animate-pulse">
-                    {message}
-                </p>
-            )}
         </div>
     );
 };
